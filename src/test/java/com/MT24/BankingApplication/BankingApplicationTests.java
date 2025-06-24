05538274192fb1811001fbbd87237d76ac2316ba
Farhantigadi <farhantigadi123@gmail.com>
2025-06-24 16:26:32 +0530

    initial commit

---
+++ src/test/java/com/MT24/BankingApplication/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MT24.BankingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
