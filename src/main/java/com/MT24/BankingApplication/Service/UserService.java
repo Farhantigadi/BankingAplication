887e9ccd1aa4d160c608668736d3affcdef6486e
Farhantigadi <farhantigadi123@gmail.com>
2025-07-07 12:25:50 +0530

    another test case ACTIONS pass

---
+++ src/main/java/com/MT24/BankingApplication/Service/UserService.java
@@ -0,0 +1,24 @@
+package com.MT24.BankingApplication.Service;
+
+import com.MT24.BankingApplication.Dto.LoginResponseDto;
+
+
+import com.MT24.BankingApplication.Model.User;
+import com.MT24.BankingApplication.dto.UserRequestDto;
+import com.MT24.BankingApplication.dto.UserResponseDto;
+
+public interface UserService {
+    LoginResponseDto registerUser(UserRequestDto userRequestDto);
+    UserResponseDto registerAdmin(UserRequestDto dto);
+    User getUserInfo(String accountNumber);
+
+
+    String deposit(String accountNumber, Double amount);
+    String withdraw(String accountNumber, Double amount);
+
+    String transferMoney(String senderAccount, String receiverAccount, Double amount);
+    String getBalance(String accountNumber);
+
+
+}
+
